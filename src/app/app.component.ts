53a4d4c4d04a867ad86be412cf2aef82002ab449
kmv9063 <mverma@nownow.ng>
2023-04-02 23:53:01 +0530

    Consistecnty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WOCFrontEnd';
+
+  public constituency ={
+    constituency:'',
+    electedIndividual:'',
+    Party:'',
+    Age:'',
+    Gender:'',
+    score:''
+  }
+  formSubmit()
+  {
+    alert("submit");
+  }
 }
